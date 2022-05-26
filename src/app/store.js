521c137731cc45f30d2b7c1ed16707f3d6a433f1
Matteo <gnappo.1@live.it>
2022-05-25 18:55:57 -0700

    create store and prepare import for counterReducer

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from 'redux-starter-kit';
+// import counterReducer from "../features/counter/CounterSlice"
+
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+})
